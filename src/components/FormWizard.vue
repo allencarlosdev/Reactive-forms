7b3ccfbc13bf791c09e776d25c567c50088fff12
Carlos Allen <allencarlosdev@gmail.com>
2022-10-19 10:24:08 -0400

    Passing the data to the order component

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -3,7 +3,7 @@
     <FormPlanPicker v-if="currentStepNumber === 1" @update="processStep"/>
     <FormUserDetails v-if="currentStepNumber === 2" @update="processStep"/>
     <FormAddress v-if="currentStepNumber === 3" @update="processStep"/>
-    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep"/>
+    <FormReviewOrder v-if="currentStepNumber === 4" @update="processStep" :wizard-data="form"/>
 
     <div class="progress-bar">
       <div :style="`width: ${progress}%;`"></div>
@@ -19,6 +19,7 @@
       </button>
       <button
         @click="goNext"
+        :disabled="!canGoNext"
         class="btn"
       >Next</button>
     </div>
@@ -43,6 +44,7 @@ export default {
   data () {
     return {
       currentStepNumber: 1,
+      canGoNext: false,
       length: 4,
       form: {
         plan: null,
@@ -64,12 +66,14 @@ export default {
   methods: {
     processStep(stepData){
       Object.assign(this.form, stepData)
+      this.canGoNext = true
     },
     goBack () {
       this.currentStepNumber--
     },
     goNext () {
       this.currentStepNumber++
+      this.canGoNext = false 
     }
   }
 }
