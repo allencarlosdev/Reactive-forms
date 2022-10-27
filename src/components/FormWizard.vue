3becffa7fdc3ce3c258c6526ca96ad792b9ba6b9
Carlos Allen <allencarlosdev@gmail.com>
2022-10-27 11:54:17 -0400

    Placing preloader after the form

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="wizardInProgress">
+    <div v-if="wizardInProgress" v-show="asyncState !== 'pending'">
       <keep-alive>
         <component 
           ref="currentStep"
@@ -42,6 +42,12 @@
           <a href="https://allencarlosdev.com" target="_blank" class="btn"> Go somewhere cool!</a>
         </p>
     </div>
+    <div class="loading-wrapper" v-if="asyncState === 'pending'">
+      <div class="loader">
+        <img src="/spinner.svg" alt="">
+        <p>Please wait, we're hitting our servers!</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -63,6 +69,7 @@ export default {
     return {
       currentStepNumber: 1,
       canGoNext: false,
+      asyncState: null,
       steps:[
         'FormPlanPicker',
         'FormUserDetails',
@@ -100,8 +107,10 @@ export default {
   },
   methods: {
     submitOrder(){
+      this.asyncState = 'pending'
       postFormToDB(this.form)
         console.log('form submitted', this.form)
+        this.asyncState = 'success'
         this.currentStepNumber++
     },
     nextButtonAction(){
